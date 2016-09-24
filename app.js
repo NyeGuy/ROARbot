31945dd34fd81885f4a043e0fbd10fd1007550b7
Nye Warburton <nyewarburton@gmail.com>
2016-09-23 19:58:19 -0700

    another attempt to get the server running

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// use twilio, connect with user keys and use cron
+var twilio = require('twilio'),
+    client = twilio('AC513e87066a1e90d73e86ea276a800804', 'f3145ed7b367bf8e00d474795036cc58'),
+    cronJob = require('cron').CronJob;
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
-
-// Process application/json
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.set('port', (process.env.PORT || 5000));
 
 // Index route
 app.get('/', function (req, res) {
@@ -19,16 +23,32 @@ app.get('/', function (req, res) {
 
 // Spin up the server
 app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
+    console.log('running on port',app.get('port'))
 });
 
-// use twilio, connect with user keys and use cron
-//var twilio = require('twilio'),
-   // client = twilio('AC513e87066a1e90d73e86ea276a800804', //'f3145ed7b367bf8e00d474795036cc58'),
-   // cronJob = require('cron').CronJob;
+//adding route for /message using TwiML
+//app.post('/message', function (req, res) {
+//  var resp = new twilio.TwimlResponse();
+//  if( req.body.Body.trim().toLowerCase() === 'roar' ) {
+//    var fromNum = req.body.From;
+//    if(numbers.indexOf(fromNum) !== -1) {
+//      resp.message('You already subscribed!');
+//    } else {
+//      resp.message('Thank you, you will now be ROARed at! Reply "STOP" to stop //receiving updates.');
+  //    usersRef.push(fromNum);
+//    }
+//  } else {
+//    resp.message('Welcome to ROARbot. Text "ROAR" to begin.');
+//  }
+//  res.writeHead(200, {
+//    'Content-Type':'text/xml'
+//  });
+//  res.end(resp.toString());
+//});
+
 
 // cron - fire off at 7:15
-//var textJob = new cronJob( '09 19 * * *', function(){
- // client.sendMessage( { to:'3108898908', from:'4244656385', body:'Roar!' }, //function( err, data ) {});
-//},  null, true);
+var textJob = new cronJob('35 19 * * *', function(){
+    client.sendMessage({to:'3108898908',from:'4244656385',body:'Roar!'}, function(err, data ) {});
+},  null, true);
 
--- twilio.js
+++ twilio.js
@@ -1,73 +1,54 @@
 var twilio = require('twilio'),
     client = twilio('AC513e87066a1e90d73e86ea276a800804', 'f3145ed7b367bf8e00d474795036cc58'),
     cronJob = require('cron').CronJob;
-
-'use strict'
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-
-app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json())
-
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
-
-//firebase
+ 
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+ 
 var Firebase = require('firebase'),
     usersRef = new Firebase('https://gamebotdb.firebaseio.com/Users/');
-
-//numbers we want to send - will be stored in firebase
-var numbers = [];
-
-//adding new users to firebase
+ 
+var numbers = ['3108898908'];
+ 
 usersRef.on('child_added', function(snapshot) {
   numbers.push( snapshot.val() );
   console.log( 'Added number ' + snapshot.val() );
 });
-
-//fire off cron to the numbers declared
-var textJob = new cronJob( '50 09 * * *', function(){
-for( var i = 0; i < numbers.length; i++ ) {
-  client.sendMessage( { to:numbers[i], from:'4244656385', body:'Roar!'}, function( err, data ) {
-    console.log( data.body );
-  });
-}},  null, true);
-
-
-//adding route for /message using TwiML
+ 
+var textJob = new cronJob( '10 20 * * *', function(){
+  for( var i = 0; i < numbers.length; i++ ) {
+    client.sendMessage( { to:numbers[i], from:'4244656385', body:'Roar!'}, function( err, data ) {
+      console.log( data.body );
+    });
+  }
+},  null, true);
+ 
 app.post('/message', function (req, res) {
   var resp = new twilio.TwimlResponse();
-  if( req.body.Body.trim().toLowerCase() === 'roar' ) {
+  if( req.body.Body.trim().toLowerCase() === 'subscribe' ) {
     var fromNum = req.body.From;
     if(numbers.indexOf(fromNum) !== -1) {
       resp.message('You already subscribed!');
     } else {
-      resp.message('Thank you, you will now be ROARed at! Reply "STOP" to stop receiving updates.');
+      resp.message('Thank you, you are now subscribed. Reply "STOP" to stop receiving updates.');
       usersRef.push(fromNum);
     }
   } else {
-    resp.message('Welcome to ROARbot. Text "ROAR" to begin.');
+    resp.message('Welcome to Daily Updates. Text "Subscribe" receive updates.');
   }
   res.writeHead(200, {
     'Content-Type':'text/xml'
   });
   res.end(resp.toString());
 });
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
-
+ 
+var server = app.listen(8000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
 
 
