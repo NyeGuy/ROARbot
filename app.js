3d06d70fb0e83c3b9a6b9d4e7337c7f0930b9032
Nye Warburton <nyewarburton@gmail.com>
2016-09-23 18:55:13 -0700

    updated server code

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+bodyParser = require('body-parser'),
+app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+var twilio = require('twilio'),
+    client = twilio('AC513e87066a1e90d73e86ea276a800804', 'f3145ed7b367bf8e00d474795036cc58'),
+    cronJob = require('cron').CronJob;
 
-app.use(express.static(__dirname + '/public'));
-var port= process.env.PORT || 8000;
-app.listen(port);
+//Static App use - for Website
+//app.use(express.static(__dirname + '/public'));
+//var port= process.env.PORT || 8000;
+//app.listen(port);
+
+var textJob = new cronJob( '55 18 * * *', function(){
+  client.sendMessage( { to:'3108898908', from:'4244656385', body:'Roar!' }, function( err, data ) {});
+},  null, true);
+
+var server = app.listen(8000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
+
