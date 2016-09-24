9c30680f9285035afd6680b70d5265ef78f22ae1
Nye Warburton <nyewarburton@gmail.com>
2016-09-23 19:08:23 -0700

    // for testing

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ app.listen(app.get('port'), function() {
 });
 
 // use twilio, connect with user keys and use cron
-var twilio = require('twilio'),
-    client = twilio('AC513e87066a1e90d73e86ea276a800804', 'f3145ed7b367bf8e00d474795036cc58'),
-    cronJob = require('cron').CronJob;
+//var twilio = require('twilio'),
+   // client = twilio('AC513e87066a1e90d73e86ea276a800804', //'f3145ed7b367bf8e00d474795036cc58'),
+   // cronJob = require('cron').CronJob;
 
 // cron - fire off at 7:15
-var textJob = new cronJob( '09 19 * * *', function(){
-  client.sendMessage( { to:'3108898908', from:'4244656385', body:'Roar!' }, function( err, data ) {});
-},  null, true);
+//var textJob = new cronJob( '09 19 * * *', function(){
+ // client.sendMessage( { to:'3108898908', from:'4244656385', body:'Roar!' }, //function( err, data ) {});
+//},  null, true);
 
